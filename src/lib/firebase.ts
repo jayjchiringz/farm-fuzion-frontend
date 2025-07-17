0eab5b5e48f34389d03444e3b45cd6b007f268fb
jayjchiringz <jayjchiringz@gmail.com>
2025-07-17 20:24:40 +0300

    Farm Fuzion Frontend: Adjusted admin Updated firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDn89DTHUQjFWCV6J3wm1EMP3B-227qJSE",
   authDomain: "farm-fuzion-abdf3.firebaseapp.com",
   projectId: "farm-fuzion-abdf3",
-  storageBucket: "farm-fuzion-abdf3.appspot.com", // ✅ FIXED!
+  storageBucket: "farm-fuzion-abdf3.firebasestorage.app",
   messagingSenderId: "602008321992",
   appId: "1:602008321992:web:27b433f1e69fbac6a8cafd",
   measurementId: "G-X0QZDW0MVE"

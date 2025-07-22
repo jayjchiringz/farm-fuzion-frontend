1161d83715d1de0a5a5146581df1fce46629a8c6
jayjchiringz <jayjchiringz@gmail.com>
2025-07-23 01:38:48 +0300

    Farm Fuzion Frontend: Removed piecharts from dshbord

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -608,11 +608,11 @@ export default function AdminSidebar({ children }: { children: React.ReactNode }
             </h1>
           </div>
           <div className="flex flex-wrap gap-6">
-            {/*}
             <OverviewStats
               totalGroups={stats.totalGroups}
               totalFarmers={stats.totalFarmers}
             />
+            {/*}
             <GroupStats statusCounts={stats.statusCounts} />
             <FarmerStats farmerByGroup={stats.farmerByGroup} />
             */}

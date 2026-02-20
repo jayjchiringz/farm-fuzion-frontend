5d10f2dc073ce3f9735577e1e38c7736f364251f
jayjchiringz <jayjchiringz@gmail.com>
2026-02-20 19:38:29 +0300

    Frontend: improved farmers dashboard --2

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -215,7 +215,7 @@ export default function Dashboard() {
         {/* Main Content */}
         <main className="flex-1 overflow-y-auto bg-slate-50 dark:bg-brand-dark text-brand-dark dark:text-brand-apple p-6 md:p-8 transition-colors duration-300">
           <h1 className="text-[46px] leading-[64px] font-bold mb-4 font-ubuntu">
-            {fullName}'s Farm Dashboard
+            {fullName}'s Farm
           </h1>
 
           {/* Farmer's Key Metrics */}
